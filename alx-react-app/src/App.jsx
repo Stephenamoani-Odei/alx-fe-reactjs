56adf2a977ca96b3a2b3479f86ea47e26167104a
STEPHEN ODEI AMOANI <odeiamoani98@gmail.com>
2025-09-01 09:11:41 -0700

    initial commit

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+      <div>
+      <WelcomeMessage/>
+      < MainContent/>
+      
+      < UserProfile
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography"
+      />
+      <Footer/>
+      </div>    
+
+  )
+}
+
+export default App
