4f97b9ee38169bc7dacf91229d5b31ac34fd6bd0
STEPHEN ODEI AMOANI <odeiamoani98@gmail.com>
2025-09-18 13:34:02 -0700

    Add files via upload

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<><RecipeList /><AddRecipeForm /></>} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/edit/:id" element={<EditRecipeForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
