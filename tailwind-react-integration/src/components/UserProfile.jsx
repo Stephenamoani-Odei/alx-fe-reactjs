5850ae17fedd57fdf681d082c0c466a4dbb791bc
STEPHEN ODEI AMOANI <odeiamoani98@gmail.com>
2025-10-04 08:12:45 -0700

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@
 function UserProfile() {
   return (
     <div class="bg-grey-100 p-8 max-w-sm mx-auto, my-20 round-lg shadow-lg  md:p-8 sm:p-4 max-w-xs max-w-sm md:max-w-sm hover:shadow-xl"  className="user-profile">
-      <img clas="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 sm:w-36 h-36 hover:scale-110 transition-transform duration-300 ease-in-out" src="https://via.placeholder.com/150" alt="User" />
+      <img clas="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 sm:w-36 h-36 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out" src="https://via.placeholder.com/150" alt="User" />
       <h1 class="text-xl text-blue-800 my-4  sm:text-lg md: text-xl hover:text-blue-500">John Doe</h1>
       <p class="text-grey-600 text-base sm:text-sm md:text-base md:text-xl">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
@@ -11,6 +11,7 @@ function UserProfile() {
 
 
 export default UserProfile;
+
 
 
 
